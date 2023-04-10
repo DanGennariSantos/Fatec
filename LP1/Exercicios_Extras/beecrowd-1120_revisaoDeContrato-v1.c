91017a1b72ee3b406136c58c5cad66e8161c3352
Danilo Gennari dos Santos <125101328+DanGennariSantos@users.noreply.github.com>
2023-04-10 16:42:25 -0300

    Update beecrowd-1120_revisaoDeContrato-v1.c

--- LP1/Exercicios_Extras/beecrowd-1120_revisaoDeContrato-v1.c
+++ LP1/Exercicios_Extras/beecrowd-1120_revisaoDeContrato-v1.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>>
+#include <stdlib.h>
 
-void removeDigito(char texto1[]){
-
+void removeDigito(char texto1[]) {
     int tamanho = strlen(texto1) - 2;
-    char defeito[2] = {texto1[0], '\0'};
-    char texto2[tamanho + 1];
+    char defeito = texto1[0];
+    char texto2[tamanho];
     int i;
     int j = 0;
 
-    for (i = 2; i <= tamanho; i++ )
-    {
-        char numTemp[2] = {texto1[i], '\0'};
-
-        if ((atoll(numTemp)) != atoll(defeito))
-        {
+    for (i = 2; i <= tamanho; i++) {
+        if (texto1[i] != defeito) {
            texto2[j] = texto1[i];
            j++;
         }
@@ -23,29 +18,49 @@ void removeDigito(char texto1[]){
 
     texto2[j] = '\0';
 
-    long long int saida = atoll (texto2);
-    printf("Valor corrigido: ");
-    printf("%lld\n", saida);
+    if (texto2[0] == '\0' || strcmp(texto2, "0") == 0) {
+        printf("Valor corrigido: 0\n");
+    } else {
+        if (texto2[0] != '0') {
+            printf("Valor corrigido: %s\n", texto2);
+        } else {
+            int posicao = 0;
+            while (texto2[posicao] == '0' || texto2[posicao] == '\0') {
+                posicao++;
+            }
+            int novoTamanho = strlen(texto2) + 1;
+            char saida[novoTamanho];
+            int k = 0;
+            for (i = posicao; i < j; i++) {
+                saida[k] = texto2[i];
+                k++;
+            }
+            saida[k] = '\0';
+            
+            if (strcmp(saida, "") == 0) {
+            printf("Valor corrigido: 0\n");}
+            
+            else{
+            printf("Valor corrigido: %s\n", saida);} 
+            
+        }
+    }
 }
 
-
-int main(void)
-{
-    char entrada[203];
-    char fim[] = "0 0\n";
+int main(void) {
+    char entrada[103];
+    char fim[5] = "0 0\n";
 
     printf("Insira o digito que deu defeito e o respectivo valor: ");
     fgets(entrada, sizeof(entrada), stdin);
 
-
-    while ((strcmp(entrada, fim)) != 0)
-        {
-        removeDigito (entrada);
+    while ((strcmp(entrada, fim)) != 0) {
+        removeDigito(entrada);
 
         printf("Insira o digito que deu defeito e o respectivo valor: ");
         fgets(entrada, sizeof(entrada), stdin);
-        }
+    }
 
-    printf ("\nPrograma encerrado...\n");
+    printf("\nPrograma encerrado...\n");
     return 0;
 }
